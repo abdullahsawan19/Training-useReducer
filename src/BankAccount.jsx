327c4cf73cae197f8669634a971167f0e2c99b56
Abdullah Sawan <abdullahsawan19@gmail.com>
2025-12-19 02:00:52 +0200

    BankAccountMangment

--- src/BankAccount.jsx
+++ src/BankAccount.jsx
@@ -1,67 +1,226 @@
 import { useReducer } from "react";
 
-/*
-INSTRUCTIONS / CONSIDERATIONS:
-
-1. Let's implement a simple bank account! It's similar to the example that I used as an analogy to explain how useReducer works, but it's simplified (we're not using account numbers here)
-
-2. Use a reducer to model the following state transitions: openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount. Use the `initialState` below to get started.
-
-3. All operations (expect for opening account) can only be performed if isActive is true. If it's not, just return the original state object. You can check this right at the beginning of the reducer
-
-4. When the account is opened, isActive is set to true. There is also a minimum deposit amount of 500 to open an account (which means that the balance will start at 500)
-
-5. Customer can only request a loan if there is no loan yet. If that condition is met, the requested amount will be registered in the 'loan' state, and it will be added to the balance. If the condition is not met, just return the current state
-
-6. When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point)
-
-7. Customer can only close an account if there is no loan, AND if the balance is zero. If this condition is not met, just return the state. If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the initial state
-*/
-
 const initialState = {
   balance: 0,
   loan: 0,
   isActive: false,
+  startDepositAmount: 500,
+  depositAmount: "",
+  withdrawAmount: "",
+  loanAmount: "",
+  payLoanAmount: "",
 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "openAccount":
+      return {
+        ...state,
+        isActive: true,
+        balance: state.startDepositAmount,
+      };
+
+    case "setStartAmount":
+      return {
+        ...state,
+        startDepositAmount: Number(action.payload),
+      };
+
+    case "deposit":
+      return {
+        ...state,
+        balance: state.balance + state.depositAmount,
+        depositAmount: "",
+      };
+
+    case "setDepositAmount":
+      return {
+        ...state,
+        depositAmount: Number(action.payload),
+      };
+
+    case "withdraw":
+      return {
+        ...state,
+        balance: state.balance - state.withdrawAmount,
+        withdrawAmount: "",
+      };
+
+    case "setWithdrawAmount":
+      return {
+        ...state,
+        withdrawAmount: Number(action.payload),
+      };
+
+    case "requestLoan":
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        balance: state.balance + state.loanAmount,
+        loan: state.loanAmount,
+        loanAmount: "",
+      };
+
+    case "setLoanAmount":
+      return {
+        ...state,
+        loanAmount: Number(action.payload),
+      };
+
+    case "payLoan":
+      if (
+        state.payLoanAmount > state.loan ||
+        state.payLoanAmount > state.balance
+      )
+        return state;
+      return {
+        ...state,
+        balance: state.balance - state.payLoanAmount,
+        loan: state.loan - state.payLoanAmount,
+        payLoanAmount: "",
+      };
+
+    case "setPayLoanAmount":
+      if (state.loan === 0) return state;
+      return {
+        ...state,
+        payLoanAmount: Number(action.payload),
+      };
+
+    case "closeAccount":
+      if (state.loan > 0 || state.balance !== 0) {
+        alert("لا يمكن إغلاق الحساب! تأكد من سداد القروض وسحب الرصيد.");
+        return state;
+      }
+      return initialState;
+
+    default:
+      return state;
+  }
+}
+
 function BankAccount() {
+  const [
+    {
+      balance,
+      loan,
+      isActive,
+      startDepositAmount,
+      depositAmount,
+      withdrawAmount,
+      loanAmount,
+      payLoanAmount,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
       <h1>useReducer Bank Account</h1>
-      <p>Balance: X</p>
-      <p>Loan: X</p>
+      <p>Balance: {balance}</p>
+      <p>Loan: {loan}</p>
 
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button
+          onClick={() => dispatch({ type: "openAccount" })}
+          disabled={isActive}
+        >
           Open account
         </button>
-        <input type="number" placeholder="Open account with ?" />
+
+        <input
+          type="number"
+          placeholder="Open account with ?"
+          value={startDepositAmount}
+          onChange={(e) =>
+            dispatch({ type: "setStartAmount", payload: e.target.value })
+          }
+          disabled={isActive}
+        />
       </p>
+
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button
+          onClick={() => {
+            dispatch({ type: "deposit" });
+          }}
+          disabled={!isActive}
+        >
           Deposit
         </button>
-        <input type="number" placeholder="Initial deposit" />
+        <input
+          type="number"
+          placeholder="Initial deposit"
+          value={depositAmount}
+          onChange={(e) =>
+            dispatch({ type: "setDepositAmount", payload: e.target.value })
+          }
+        />
       </p>
+
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button
+          onClick={() => {
+            dispatch({ type: "withdraw" });
+          }}
+          disabled={!isActive}
+        >
           Withdraw
         </button>
-        <input type="number" placeholder="Withdraw amount" />
+        <input
+          type="number"
+          placeholder="Withdraw amount"
+          value={withdrawAmount}
+          onChange={(e) =>
+            dispatch({ type: "setWithdrawAmount", payload: e.target.value })
+          }
+        />
       </p>
+
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button
+          onClick={() => {
+            dispatch({ type: "requestLoan" });
+          }}
+          disabled={!isActive}
+        >
           Request a loan of
         </button>
-        <input type="number" placeholder="Request a loan of ?" />
+        <input
+          type="number"
+          placeholder="Request a loan of ?"
+          value={loanAmount}
+          onChange={(e) =>
+            dispatch({ type: "setLoanAmount", payload: e.target.value })
+          }
+        />
       </p>
+
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button
+          onClick={() => {
+            dispatch({ type: "payLoan" });
+          }}
+          disabled={!isActive}
+        >
           Pay loan
         </button>
-        <input type="number" placeholder="Pay loan of ?" />
+        <input
+          type="number"
+          placeholder="Pay loan of ?"
+          value={payLoanAmount}
+          onChange={(e) =>
+            dispatch({ type: "setPayLoanAmount", payload: e.target.value })
+          }
+        />
       </p>
       <p>
-        <button onClick={() => {}} disabled={false}>
+        <button
+          onClick={() => {
+            dispatch({ type: "closeAccount" });
+          }}
+          disabled={!isActive}
+        >
           Close account
         </button>
       </p>
